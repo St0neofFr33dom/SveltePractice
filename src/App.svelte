d96c6dea767838ca70148ea41701b07a7944e2c2
St0neofFr33dom <christophefreeston@googlemail.com>
2022-11-16 12:40:45 +0000

    Made hero image component, established props that have a default value and dynamically change when data is retireved from the fetch request to the poekapi

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,22 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import HeroImage from "./lib/HeroImage.svelte";
+  let data 
+  $: fetchedImage = {url: (data) ? data.sprites.other.home.front_default : "https://purepng.com/public/uploads/large/purepng.com-pokeballpokeballdevicepokemon-ballpokemon-capture-ball-17015278259020osdb.png", alt: (data) ? data.name : "Pokeball"}
+  async function fetchData(){
+    let response = await fetch("https://pokeapi.co/api/v2/pokemon/sunkern")
+    data = await response.json()
+    // fetchedImage = {url:data.sprites.other.home.front_default, alt:data.name}
+    console.log(data)
+    return
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <h1>Pokemon App</h1>
+  <HeroImage heroImage={fetchedImage} />
+  <button on:click={fetchData}>Get</button>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
---
+++ src/lib/HeroImage.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let heroImage
+</script>
+
+<div>
+    <img src={heroImage.url} alt={heroImage.alt} title={heroImage.alt}/>
+</div>
+
+<style>
+    img{
+        width: 30vw;
+        height: auto;
+    }
+
+    </style>

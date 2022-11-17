cbe02ddd91cc3ca78e9f093af1871cb879947817
St0neofFr33dom <christophefreeston@googlemail.com>
2022-11-17 10:54:04 +0000

    Started the autocomplete function, able to pass in idfferent arrays to autocomplete, after 2 letters inputted into the search bar, the first 5 matches appear. Will start working on making the selections clickable, making the space where the predictions appear stable and might remove the limit of words found as it seems to cycle through the arrays fast enough

---
+++ src/funcs/autoComplete.js
@@ -0,0 +1,13 @@
+export default function autoComplete(value,array){
+    let predictedArray = []
+    for (let i = 0; i < array.length; i++){
+        if (array[i].substr(0, value.length).toUpperCase() == value.toUpperCase()) {
+            let matchedWord = array[i].substr(0, value.length);
+            matchedWord.toUpperCase()
+            matchedWord += array[i].substr(value.length);
+            predictedArray.push(matchedWord)
+        }
+        if (predictedArray.length >= 5){return predictedArray}
+    }
+    return predictedArray
+}
--- src/lib/SearchForm.svelte
+++ src/lib/SearchForm.svelte
@@ -1,17 +1,28 @@
 <script>
     import capitalise from "../funcs/capitalise.js"
+    import autoComplete from "../funcs/autoComplete.js"
+    import pokemonArray from "../data/pokemon.js"
+    import abilityArray from "../data/abilities.js"
+    import moveArray from "../data/moves.js"
+
     let baseURL = "https://pokeapi.co/api/v2/"
     let searchTerm = ["pokemon","type","ability","move"]
     let searchCategory = searchTerm[0]
-    let types = ["normal","grass","fire","water","electricity","flying","bug","poison","rock","ground","fighting","psychic","ghost","ice","dragon","dark","steel","fairy"]
+    let typeArray = ["normal","grass","fire","water","electricity","flying","bug","poison","rock","ground","fighting","psychic","ghost","ice","dragon","dark","steel","fairy"]
     let searchValue = ""
+    let arraySelection = {pokemon:pokemonArray,ability:abilityArray,type:typeArray,move:moveArray}
+    $: predictedArray = (searchValue.length > 1) ? autoComplete(searchValue,arraySelection[searchCategory]) : []
     $: fullUrl = baseURL+searchCategory+searchValue
+
+    function handleChange(){
+        searchValue = ""
+    }
 </script>
 
 <div class="searchForm">
     <div>
         <label for="searchTerms">Search By:</label>
-        <select bind:value={searchCategory} name="searchTerms" id="searchTerms">
+        <select bind:value={searchCategory} name="searchTerms" id="searchTerms" on:change={handleChange}>
             {#each searchTerm as term}
                 <option value={term}>{capitalise(term)}</option>
             {/each}
@@ -19,17 +30,19 @@
     </div>
     <div>
         <label for="searchBar">{capitalise(searchCategory)}:</label>
-        {#if searchCategory === "type"}
-        <select bind:value={searchValue} name="types" id="types">
-            {#each types as type}
-                <option value={type}>{capitalise(type)}</option>
+        <div class="barAndAutoComplete">
+            <input id="searchBar" bind:value={searchValue}/>
+            {#if searchValue.length > 1}
+            {#each predictedArray as suggestion}
+                <div>{suggestion}</div>
             {/each}
-        </select>
-        {:else}
-        <input id="searchBar" bind:value={searchValue}/>
         {/if}
+        </div>
     </div>
-    <button on:click={()=>{console.log(searchCategory)}}>Test</button>
+    <button on:click={()=>{
+        console.log(searchValue)
+        console.log(predictedArray)}
+    }>Test</button>
 </div>
 
 <style>
@@ -39,5 +52,9 @@
         padding: 1em;
         gap: 1em;
         border: black 2px solid;
+    }
+    .barAndAutoComplete{
+        display:flex;
+        flex-direction: column;
     }
 </style>
